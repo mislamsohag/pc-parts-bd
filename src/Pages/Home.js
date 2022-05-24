0c3fb4ec45a7e63506f2f76aeb4cb2537ac22ea6
soahg <mislamsohag@gmail.com>
2022-05-24 11:53:24 +0600

    slider Add

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Slider from '../Componants/Slider';
 
 const Home = () => {
     return (
         <div>
+            <Slider></Slider>
             <h1 className='text-3xl'>Home Page</h1>
         </div>
     );
