2c98274fc0a6bf575572bc4d1f841a3d151addc2
soahg <mislamsohag@gmail.com>
2022-05-24 09:35:28 +0600

    link updatet

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About';
 import Home from './Pages/Home';
-import Navbar from './Pages/Navbar';
+import Navbar from './Componants/Navbar';
 import Reviews from './Pages/Reviews';
 import Blogs from './Pages/Blogs';
 import Login from './Pages/Login';
+import MyReview from './Pages/MyReview';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
             <Route path='/' element={<Home></Home>}></Route>
             <Route path='/home' element={<Home></Home>}></Route>
             <Route path='/about' element={<About></About>}></Route>
-            <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+            <Route path='/reviews' element={<Reviews></Reviews>}>
+
+            </Route>
             <Route path='/blogs' element={<Blogs></Blogs>}></Route>
             <Route path='/login' element={<Login></Login>}></Route>
           </Routes>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        light: {
+
+          "primary": "#28A9A8",
+          "secondary": "#00033E",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+      "dark",
+    ],
   },
   plugins: [require("daisyui")],
 }
