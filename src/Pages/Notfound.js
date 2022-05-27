8f7abf514a72652d24401c3c0664b89917892c5a
soahg <mislamsohag@gmail.com>
2022-05-27 17:38:49 +0600

    My reviews Done

--- src/Pages/Notfound.js
+++ src/Pages/Notfound.js
@@ -3,24 +3,24 @@ import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <section className='container text-center '>
+    <section className='text-center'>
       <div className='row vh-50 my-5'>
-        <div className='col-sm-12 col-md6 col-lg-4 g-3'>
-          <h1 className='text  text-danger'>
+        <div className='col-sm-12 col-md-6 col-lg-4 gap-3'>
+          <h1 className='text  text-red-600'>
             Error 404
           </h1>
-          <h1 className=''>
+          <h1 className='font-mono'>
             Ooops! You weren't supposed to de this looking for isn't here.
           </h1>
-          <p className=''>
+          <p className='font-mono'>
             the page you're looking for no longer exists. Return to the home page and remember: you haven't see any things
           </p>
-          <div className='d-flex justify-content-evenly m-3'>
+          <div className='flex justify-evenly gap-3 m-5'>
             <Link
               to='/'
               className='btn btn-info text-black '
             >
-              Back to homepage
+              Back to home
             </Link>
             <Link
               to='/about'
@@ -30,8 +30,8 @@ const NotFound = () => {
             </Link>
           </div>
         </div>
-        <div className='col-sm-12 col-md6 col-lg-8'>
-          <div className='rounded'>
+        <div className='col-sm-12 col-md-6 mx-auto col-lg-8'>
+          <div className='rounded flex justify-center items-center'>
             <img className='img-fluid'
               src='https://i.ibb.co/c6ww58q/404-image.jpg'
               alt=''
--- src/Pages/Reviews/MyReview.js
+++ src/Pages/Reviews/MyReview.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../Componants/Footer';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
 
 const MyReview = () => {
+    const [user] = useAuthState(auth);
+    const [myReview, setMyReview] = useState([]);
 
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/review?userEmail=${user.email}`)
+                .then(res => res.json())
+                .then(data => setMyReview(data))
+        }
+    }, [user])
+
+    // console.log(myReview);
 
     return (
         <div>
-            <h2 className="text-3xl">My Review Page</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Review</th>
+                            <th>Ratting</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        {
+                            myReview.map((review, index) => <tr key={index}>
+
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="P. Image">
+                                            <div className="mask mask-circle w-12 h-12">
+                                                <img src={review.productImg} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {review.comment}
+                                </td>
+                                <td>{review.rating}</td>
+                                <td><button className="btn btn-ghost btn-xs">Update</button></td>
+                                <td>
+                                    <button className="btn btn-ghost btn-xs">Delete</button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
 
             <Footer></Footer>
         </div>
