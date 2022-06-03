c0d675e6d755ae18fdd57305b8c574b980bf0c95
soahg <mislamsohag@gmail.com>
2022-06-03 07:22:10 +0600

    orders

--- src/Dashboard/MyOrders.js
+++ src/Dashboard/MyOrders.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { signOut } from 'firebase/auth';
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+    const [myOrder, setMyOrder] = useState([]);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/my-orders?userEmail=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+
+                    setMyOrder(data);
+                });
+        }
+    }, [user])
+
+
     return (
         <div>
-            <h2>My Orders Page</h2>
+            <h2>My Orders Page : {myOrder.length}</h2>
         </div>
     );
 };
